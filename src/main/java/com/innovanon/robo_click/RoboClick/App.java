c72bad787c9819abd733c9ea5f6af97d90bafcb1
Juju Gouldbergstein <gouldbergstein@durga.innovanon.com>
2018-08-05 00:08:36 -0500

    migrating to maven

---
+++ src/main/java/com/innovanon/robo_click/RoboClick/App.java
@@ -0,0 +1,13 @@
+package com.innovanon.robo_click.RoboClick;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
