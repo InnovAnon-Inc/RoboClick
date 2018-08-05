4b8934e015ccbd8936aa82a15597bab06fbfcbd6
Juju Gouldbergstein <gouldbergstein@durga.innovanon.com>
2018-08-05 01:10:02 -0500

    depending on Simon

--- src/main/java/com/innovanon/robo_click/RoboClick/App.java
+++ src/main/java/com/innovanon/robo_click/RoboClick/App.java
@@ -1,8 +1,6 @@
 package com.innovanon.robo_click.RoboClick;
 
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
 
 /**
  * Hello world!
@@ -15,16 +13,7 @@ public class App
 	 * @throws AWTException
 	 */
 	public static void main(String[] args) throws AWTException {
-		// TODO Auto-generated method stub
-		Robot robot = new Robot();
-
-		while (true) {
-			robot.delay(1000 * 30);
-			System.out.println("press button");
-			robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-			robot.delay(500);
-			robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-			System.out.println("release button");
-		}
+		RandRoboClick rc = new RandRoboClick();
+		rc.doClicks();
 	}
 }
